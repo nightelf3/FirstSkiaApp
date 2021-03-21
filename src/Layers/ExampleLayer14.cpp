5b9a653a474fe16cfab1085fbea8e825b2b67935
Vladyslav Yeremeychuk <vyere@softserveinc.com>
2021-03-21 07:38:11 +0200

    Add example with blur mask

--- src/Layers/ExampleLayer14.cpp
+++ src/Layers/ExampleLayer14.cpp
@@ -1,7 +1,27 @@
 #include "include/Layers/ExampleLayer14.h"
+#include "include/Layers/Utils/Utils.h"
+#include "include/core/SkMaskFilter.h"
+#include "include/core/SkTextBlob.h"
+
+ExampleLayer14::ExampleLayer14()
+{
+}
 
 void ExampleLayer14::Draw(SkCanvas* canvas)
 {
 	// clear canvas with black color
 	canvas->clear(SkColors::kBlack);
+
+	SkPaint paint;
+	paint.setColor(SkColors::kWhite);
+	if (m_bEnableMask)
+		paint.setMaskFilter(SkMaskFilter::MakeBlur(kNormal_SkBlurStyle, 5.0f));
+	sk_sp<SkTextBlob> blob = SkTextBlob::MakeFromString("My First Skia App", SkFont(nullptr, 120));
+	canvas->drawTextBlob(blob.get(), 200, 360, paint);
+}
+
+bool ExampleLayer14::ProcessMouse(int x, int y, InputState state, ModifierKey modifiers)
+{
+	m_bEnableMask = InputState::kUp == state;
+	return true;
 }
--- src/include/Layers/ExampleLayer14.h
+++ src/include/Layers/ExampleLayer14.h
@@ -6,8 +6,13 @@
 class ExampleLayer14 final : public BaseLayer
 {
 public:
+	ExampleLayer14();
 	std::wstring GetTitle() const override { return L"Example #14: Mask filters"; };
 	void Draw(SkCanvas* canvas) override;
+	bool ProcessMouse(int x, int y, InputState state, ModifierKey modifiers) override;
+
+protected:
+	bool m_bEnableMask = true;
 };
 
 #endif // EXAMPLELAYER14_H_EEC9FE8C_EEE8_4524_9FF1_0793BCB5DBFE
