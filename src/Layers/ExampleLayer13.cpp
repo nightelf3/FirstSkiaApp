9fca1d466f97a66b24a6380e003f814bb0f2e11e
Vladyslav Yeremeychuk <vyere@softserveinc.com>
2021-03-21 07:23:38 +0200

    Add example with combined shaders

--- src/Layers/ExampleLayer13.cpp
+++ src/Layers/ExampleLayer13.cpp
@@ -1,7 +1,20 @@
 #include "include/Layers/ExampleLayer13.h"
+#include "include/Layers/Utils/Utils.h"
+#include "include/effects/SkGradientShader.h"
+#include "include/effects/SkPerlinNoiseShader.h"
 
 void ExampleLayer13::Draw(SkCanvas* canvas)
 {
 	// clear canvas with black color
 	canvas->clear(SkColors::kBlack);
+
+	const SkRect rBounds = GetBounds(canvas);
+	SkColor colors[2] = {SK_ColorBLUE, SK_ColorYELLOW};
+	SkPaint paint;
+	paint.setShader(SkShaders::Blend(
+		SkBlendMode::kDifference,
+		SkGradientShader::MakeRadial(SkPoint::Make(rBounds.centerX(), rBounds.centerY()), std::min(rBounds.centerX(), rBounds.centerY()),
+			colors, nullptr, 2, SkTileMode::kClamp, 0, nullptr),
+		SkPerlinNoiseShader::MakeTurbulence(0.025f, 0.025f, 2, 0.0f, nullptr)));
+	canvas->drawPaint(paint);
 }
