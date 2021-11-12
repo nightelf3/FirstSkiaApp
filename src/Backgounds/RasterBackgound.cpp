d25a042852e5d39c871cde5f29255bac97a74e35
Vladyslav Yeremeichuk <vyere@softserveinc.com>
2021-11-12 14:06:52 +0200

    Use the proper name for width parameter

--- src/Backgounds/RasterBackgound.cpp
+++ src/Backgounds/RasterBackgound.cpp
@@ -11,23 +11,23 @@ RasterBackgound::~RasterBackgound()
 	Destroy();
 }
 
-sk_sp<SkSurface> RasterBackgound::CreateSurface(int widht, int height)
+sk_sp<SkSurface> RasterBackgound::CreateSurface(int width, int height)
 {
-	const size_t bmpSize = sizeof(BITMAPINFOHEADER) + widht * height * sizeof(uint32_t);
+	const size_t bmpSize = sizeof(BITMAPINFOHEADER) + width * height * sizeof(uint32_t);
 	m_SurfaceMemory.resize(bmpSize);
 
 	BITMAPINFO* bmpInfo = reinterpret_cast<BITMAPINFO*>(m_SurfaceMemory.data());
 	ZeroMemory(bmpInfo, sizeof(BITMAPINFO));
 	bmpInfo->bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-	bmpInfo->bmiHeader.biWidth = widht;
+	bmpInfo->bmiHeader.biWidth = width;
 	bmpInfo->bmiHeader.biHeight = -height; // negative means top-down bitmap. Skia draws top-down.
 	bmpInfo->bmiHeader.biPlanes = 1;
 	bmpInfo->bmiHeader.biBitCount = 32;
 	bmpInfo->bmiHeader.biCompression = BI_RGB;
 	void* pixels = bmpInfo->bmiColors;
 
-	SkImageInfo info = SkImageInfo::Make(widht, height, SkColorType::kBGRA_8888_SkColorType, kPremul_SkAlphaType);
-	return SkSurface::MakeRasterDirect(info, pixels, sizeof(uint32_t) * widht);
+	SkImageInfo info = SkImageInfo::Make(width, height, SkColorType::kBGRA_8888_SkColorType, kPremul_SkAlphaType);
+	return SkSurface::MakeRasterDirect(info, pixels, sizeof(uint32_t) * width);
 }
 
 void RasterBackgound::SwapBuffers()
